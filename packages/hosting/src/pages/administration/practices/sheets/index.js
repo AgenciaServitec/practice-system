eaf98e2393941f281a93ad602968b1b64b9f8cc2
Pablo Aquise Lozano <aquise.pablo@iestpgildaballivian.edu.pe>
2024-05-16 18:32:37 -0500

    add_sheet_5

--- packages/hosting/src/pages/administration/practices/sheets/index.js
+++ packages/hosting/src/pages/administration/practices/sheets/index.js
@@ -4,6 +4,7 @@ import { PracticesSheet1 } from "./PracticesSheet1";
 import { Sheet } from "./Sheet";
 import { PracticesSheet2 } from "./PracticesSheet2";
 import { PracticesSheet3 } from "./PracticesSheet3";
+import { PracticesSheet5 } from "./PracticesSheet5";
 
 export const Sheets = () => {
   return (
@@ -17,6 +18,13 @@ export const Sheets = () => {
       <Sheet layout="portrait">
         <PracticesSheet3 />
       </Sheet>
+
+      <Sheet layout="portrait">
+        <PracticesSheet5 />
+      </Sheet>
+
+
+
     </PDF>
   );
 };
