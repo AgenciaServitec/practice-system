ae1d3e0432b818c950a663a809091b4279b330be
noel <admin@agenciaservitec.com>
2024-05-27 14:06:48 -0500

    Updated Acl component

--- packages/hosting/src/components/ui/Acl.jsx
+++ packages/hosting/src/components/ui/Acl.jsx
@@ -1,20 +1,20 @@
 import React, { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthentication } from "../../providers";
-import * as assert from "assert";
 
 export const Acl = ({ children, name, redirect }) => {
   const navigate = useNavigate();
-
-  const { authUser } = useAuthentication();
-
-  assert(authUser, "Missing user");
-
+  const { authUser, logout } = useAuthentication();
   const [enabled, setEnabled] = useState(true);
 
   const navigateTo403 = () => {
     return navigate("/403");
   };
+
+  if (!authUser) {
+    logout();
+    return navigate("/");
+  }
 
   const isValidateAclName = (name) =>
     (authUser?.acls || []).some((acl) => acl === name);
