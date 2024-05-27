d648ce70b50b4cc0006a382f4fc7dfff0e586ace
noel <admin@agenciaservitec.com>
2024-05-27 16:36:30 -0500

    Add getBusinessPosition in utils

--- packages/hosting/src/utils/users/abstract.js
+++ packages/hosting/src/utils/users/abstract.js
@@ -1,6 +1,12 @@
 import { capitalize } from "lodash";
+import { BusinessPosition, Roles } from "../../data-list";
 
 export const fullName = (user) =>
   capitalize(
     `${user.firstName} ${user.paternalSurname} ${user.maternalSurname}`
+  );
+
+export const getBusinessPosition = (businessPositionCode) =>
+  BusinessPosition.find(
+    (bPosition) => bPosition.value === businessPositionCode
   );
