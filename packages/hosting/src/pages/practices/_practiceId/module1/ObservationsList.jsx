3f382539c4fe865bb39e412f24ce44ceb0012417
noel <admin@agenciaservitec.com>
2024-08-04 16:23:37 -0500

    Refactoring order files

---
+++ packages/hosting/src/pages/practices/_practiceId/module1/ObservationsList.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import Col from "antd/lib/col";
+import { Alert, Row } from "antd";
+import { Acl, Button, modalConfirm } from "../../../../components";
+import moment from "moment/moment";
+import { updateAnnex } from "../../../../firebase/collections/annexs";
+import styled from "styled-components";
+
+export const ObservationsList = ({ user, annex, practice }) => {
+  const {
+    observationsAcademicSupervisor = [],
+    observationsCompanyRepresentative = [],
+  } = annex;
+  const observations = {
+    company_representative: observationsCompanyRepresentative,
+    academic_supervisor: observationsAcademicSupervisor,
+  };
+
+  const resolvedObservation = async (observationId) =>
+    modalConfirm({
+      title: "¿Esta seguro que resolvió la observación?",
+      onOk: async () => {
+        const observationsList = observations[user.roleCode];
+        const observation = observationsList.find(
+          (observation) => observation.id === observationId
+        );
+
+        const oldObservations = observationsList.filter(
+          (_observation) => _observation.id !== observationId
+        );
+
+        const newObservations = [
+          ...oldObservations,
+          observation && {
+            ...observation,
+            status: "resolved",
+            updateAt: moment().format("DD/MM/YYYY HH:mm:ss"),
+          },
+        ];
+
+        await updateAnnex(practice.id, annex.id, {
+          ...(user.roleCode === "company_representative" && {
+            observationsCompanyRepresentative: newObservations,
+          }),
+          ...(user.roleCode === "academic_supervisor" && {
+            observationsAcademicSupervisor: newObservations,
+          }),
+        });
+      },
+    });
+
+  return (
+    <Container gutter={[16, 16]}>
+      <Acl name="/practices/:practiceId/annex#observations">
+        <>
+          {observationsAcademicSupervisor
+            .filter((observation) => observation.status === "pending")
+            .map((observation, index) => (
+              <Col span={24} key={index}>
+                <Alert
+                  type="warning"
+                  showIcon
+                  message="Observación"
+                  description={observation.value}
+                  action={
+                    <Acl name="/practices/:practiceId/annex#observationResolver">
+                      <Button
+                        size="small"
+                        type="primary"
+                        onClick={() => resolvedObservation(observation.id)}
+                      >
+                        Resolver
+                      </Button>
+                    </Acl>
+                  }
+                />
+              </Col>
+            ))}
+          {observationsCompanyRepresentative
+            .filter((observation) => observation.status === "pending")
+            .map((observation, index) => (
+              <Col span={24} key={index}>
+                <Alert
+                  type="warning"
+                  showIcon
+                  message="Observación"
+                  description={observation.value}
+                  action={
+                    <Acl name="/practices/:practiceId/annex#observationResolver">
+                      <Button
+                        size="small"
+                        type="primary"
+                        onClick={() => resolvedObservation(observation.id)}
+                      >
+                        Resolver
+                      </Button>
+                    </Acl>
+                  }
+                />
+              </Col>
+            ))}
+        </>
+      </Acl>
+    </Container>
+  );
+};
+
+const Container = styled(Row)`
+  width: 100%;
+`;
---
+++ packages/hosting/src/pages/practices/_practiceId/module1/annexs/annex4/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Title } from "../../../../../../components";
+import Col from "antd/lib/col";
+import Row from "antd/lib/row";
+import { Space } from "antd";
+import styled from "styled-components";
+import { Sheet2Integration } from "./Sheet2";
+import { Sheet1Integration } from "./Sheet1";
+
+export const Annex4Integration = ({
+  practice,
+  user,
+  users,
+  practitioner,
+  company,
+  annex4,
+  onSavePractice,
+}) => {
+  return (
+    <ContainerRow gutter={[16, 16]}>
+      <Col span={24}>
+        <div className="item-sheet">
+          <Space direction="vertical" style={{ width: "100%" }}>
+            <Title level={4}>Hoja 1</Title>
+            <Sheet1Integration
+              practice={practice}
+              user={user}
+              users={users}
+              practitioner={practitioner}
+              company={company}
+              annex4={annex4}
+              onSavePractice={onSavePractice}
+            />
+          </Space>
+        </div>
+      </Col>
+      <Col span={24}>
+        <div className="item-sheet">
+          <Space direction="vertical" style={{ width: "100%" }}>
+            <Title level={4}>Hoja 2</Title>
+            <Sheet2Integration
+              practice={practice}
+              user={user}
+              users={users}
+              practitioner={practitioner}
+              company={company}
+              annex4={annex4}
+              onSavePractice={onSavePractice}
+            />
+          </Space>
+        </div>
+      </Col>
+    </ContainerRow>
+  );
+};
+
+const ContainerRow = styled(Row)`
+  .item-sheet {
+    width: 100%;
+    background: #f1f0f0;
+    padding: 1em;
+    border-radius: 1em;
+  }
+  .ant-space-item {
+    width: 100%;
+  }
+`;
