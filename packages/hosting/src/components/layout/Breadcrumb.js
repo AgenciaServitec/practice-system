628af911e66c0a4545487e52e3e8486c072fa3c6
noel <admin@agenciaservitec.com>
2024-08-08 18:43:55 -0500

    Updated style in Breadcrumb

--- packages/hosting/src/components/layout/Breadcrumb.js
+++ packages/hosting/src/components/layout/Breadcrumb.js
@@ -10,7 +10,7 @@ export const BreadcrumbLayout = ({ user }) => {
     Roles.find((role) => role.roleCode === roleCode);
 
   return (
-    <Breadcrumb style={{ margin: "16px 0" }}>
+    <Breadcrumb style={{ margin: "16px 0", fontSize: 11 }}>
       <Breadcrumb.Item>
         {capitalize(findRole(user?.roleCode)?.roleName || "User")}
       </Breadcrumb.Item>
