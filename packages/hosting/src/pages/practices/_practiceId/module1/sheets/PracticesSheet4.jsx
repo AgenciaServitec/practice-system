d1f58bd5cb5d74d50c0c87134b5c681a5507ace3
noelMoriano <nmoriano26@gmail.com>
2024-08-15 18:57:44 -0500

    fix observations

--- packages/hosting/src/pages/practices/_practiceId/module1/sheets/PracticesSheet4.jsx
+++ packages/hosting/src/pages/practices/_practiceId/module1/sheets/PracticesSheet4.jsx
@@ -4,6 +4,7 @@ import { LogoGilda } from "../../../../../images";
 import { findRole, fullName, getBusinessPosition } from "../../../../../utils";
 import dayjs from "dayjs";
 import { SignatureItem } from "../../SignatureItem";
+import { Col } from "../../../../../components";
 
 export const PracticesSheet4 = ({
   practitioner,
