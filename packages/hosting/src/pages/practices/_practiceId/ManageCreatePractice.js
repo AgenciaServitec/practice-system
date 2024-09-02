f9d024f4de6acb3812c8f52fc0acdca0989ee63d
Angel Gala <galafloresangelemilio@gmail.com>
2024-09-02 14:08:20 -0500

    removed console.log

--- packages/hosting/src/pages/practices/_practiceId/ManageCreatePractice.js
+++ packages/hosting/src/pages/practices/_practiceId/ManageCreatePractice.js
@@ -206,8 +206,6 @@ const ManageCreateProduct = ({
 
   const { required, error } = useFormUtils({ errors, schema });
 
-  console.log("errors: ", errors);
-
   useEffect(() => {
     watch("moduleNumber") &&
       setValue(
