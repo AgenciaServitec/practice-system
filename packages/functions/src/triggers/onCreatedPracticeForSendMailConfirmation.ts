a457e0aaeaebf8adcbe4129f0ebae9a8e8bf8a36
noel <admin@agenciaservitec.com>
2024-09-19 00:56:13 -0500

    fix send email to created practice

--- packages/functions/src/triggers/onCreatedPracticeForSendMailConfirmation.ts
+++ packages/functions/src/triggers/onCreatedPracticeForSendMailConfirmation.ts
@@ -10,14 +10,15 @@ export const onCreatedPracticeForSendMailConfirmation: OnDocumentCreated =
 
     const p0 = fetchUser(practice.practitionerId);
     const p1 = fetchUser(practice.academicSupervisorId);
-    const p2 = fetchUser(practice.companyRepresentativeId);
+    const p2 = practice?.companyRepresentativeId
+      ? fetchUser(practice.companyRepresentativeId)
+      : undefined;
 
     const [practitioner, academicSupervisor, companyRepresentative] =
-      await Promise.all([p0, p1, p2]);
+      await Promise.all([p0, p1, p2].filter((promise) => promise));
 
     assert(practitioner, "Missing practitioner!");
     assert(academicSupervisor, "Missing academicSupervisor!");
-    assert(companyRepresentative, "Missing companyRepresentative!");
 
     const _p0 = sendMailNewPracticeEmail(practice, practitioner, practitioner);
     const _p1 = sendMailNewPracticeEmail(
@@ -25,13 +26,11 @@ export const onCreatedPracticeForSendMailConfirmation: OnDocumentCreated =
       academicSupervisor,
       practitioner
     );
-    const _p2 = sendMailNewPracticeEmail(
-      practice,
-      companyRepresentative,
-      practitioner
-    );
+    const _p2 = companyRepresentative
+      ? sendMailNewPracticeEmail(practice, companyRepresentative, practitioner)
+      : undefined;
 
-    await Promise.all([_p0, _p1, _p2]);
+    await Promise.all([_p0, _p1, _p2].filter((promise) => promise));
   };
 
 const fetchUser = async (userId: string): Promise<User | undefined> => {
