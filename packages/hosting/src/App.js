18b43a24dae24eba6a74b280937e60290ccdcb33
noelMoriano <nmoriano26@gmail.com>
2024-10-01 18:00:11 -0500

    add scripts to users

--- packages/hosting/src/App.js
+++ packages/hosting/src/App.js
@@ -6,8 +6,36 @@ import {
   GlobalDataProvider,
   VersionProvider,
 } from "./providers";
+import { usersRef } from "./firebase/collections";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const App = () => {
+  const fetchAllUsers = () => {
+    let queryRef = usersRef.where("isDeleted", "==", false);
+    return queryRef;
+  };
+
+  const [users = []] = useCollectionData(fetchAllUsers());
+
+  const searchData = (user) => {
+    return [
+      user?.id,
+      user?.studentShift,
+      user?.dni.toString(),
+      user?.email,
+      user?.paternalSurname,
+      user?.maternalSurname,
+      user?.firstName,
+    ].filter((v) => v);
+  };
+
+  const _searchData = users.map((user) => ({
+    ...user,
+    searchData: searchData(user),
+  }));
+
+  console.log(_searchData);
+
   return (
     <VersionProvider>
       <ConfigsInitializer>
