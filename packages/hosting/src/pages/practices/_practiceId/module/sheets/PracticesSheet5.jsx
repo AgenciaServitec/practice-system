b7603842a9dbd042a190a6d0078e08a00e9a4fa8
Angel Gala <galafloresangelemilio@gmail.com>
2024-11-29 19:29:31 -0500

    updated pdfs

--- packages/hosting/src/pages/practices/_practiceId/module/sheets/PracticesSheet5.jsx
+++ packages/hosting/src/pages/practices/_practiceId/module/sheets/PracticesSheet5.jsx
@@ -51,153 +51,105 @@ export const PracticesSheet5 = ({
             <h5>I. DATOS PERSONALES: </h5>
           </div>
           <div className="body__datacompany">
-            <ul>
+            <ol>
               <li>
-                <span> 1.</span>
-                <span>
-                  Apellidos y Nombres del practicante:&nbsp;
-                  <strong className="capitalize">
-                    {fullName(practitioner)}{" "}
-                  </strong>
-                </span>
+                <span>Apellidos y Nombres del practicante: </span>
+                <span className="capitalize">{fullName(practitioner)} </span>
               </li>
               <li>
-                <span> 2.</span>
-                <span>
-                  Carrera Profesional:&nbsp;
-                  <strong>{ProfessionalCareerValue}</strong>
-                </span>
+                <span>Carrera Profesional: </span>
+                <span>{ProfessionalCareerValue}</span>
               </li>
               <li>
-                <span> 3.</span>
                 <span>
-                  Modulo Tecnico Profesional N° {practice.moduleNumber}:&nbsp;
-                  <strong className="capitalize">{practice.name}</strong>
+                  Módulo Técnico Profesional N° {practice.moduleNumber}:{" "}
                 </span>
+                <span className="capitalize">{practice.name}</span>
+                <div className="div-row">
+                  <span>Duración de PPP: </span>
+                  <span>{moduleHours?.[practice?.moduleNumber] || 0}</span>
+                </div>
               </li>
               <li>
-                <span> 4.</span>
+                <span>Periodo de Evaluación: </span>
                 <span>
-                  Duracion de PPP:&nbsp;{" "}
-                  <strong>{moduleHours?.[practice?.moduleNumber] || 0}</strong>
-                </span>
-              </li>
-              <li>
-                <span> 5.</span>
-                <span>
-                  Periodo de Evaluación:&nbsp;
-                  <strong>
-                    {practice?.startDate
-                      ? dayjs(practice.startDate, "DD/MM/YYYY").format(
-                          "DD/MM/YYYY"
-                        )
-                      : "-"}
-                    &nbsp; - &nbsp;
-                    {practice?.endDate
-                      ? dayjs(practice.endDate, "DD/MM/YYYY").format(
-                          "DD/MM/YYYY"
-                        )
-                      : "-"}
-                  </strong>
-                  &nbsp;&nbsp;&nbsp; Total de horas:&nbsp;
-                  <strong>{practice.hours} horas</strong>
-                </span>
-              </li>
-              <li>
-                <span> 6.</span>
-                <span>
-                  Nombre de la Empresa o Institución: &nbsp;
-                  <br />
+                  del{" "}
                   <span className="capitalize">
-                    <strong>{company?.socialReason}</strong>
+                    {dayjs(practice.startDate, "DD/MM/YYYY").format("LL")}{" "}
+                  </span>{" "}
+                  al &nbsp;
+                  <span className="capitalize">
+                    {dayjs(practice.endDate, "DD/MM/YYYY").format("LL")}{" "}
+                  </span>{" "}
+                </span>
+                <div className="div-row">
+                  <span>Total de horas: </span>
+                  <span>{practice.hours} horas</span>
+                </div>
+              </li>
+              <li>
+                <span>Nombre de la Empresa o Institución: </span>
+                <span className="capitalize">{company?.socialReason}</span>
+              </li>
+              <li>
+                <span>Giro de la Empresa o Institución: </span>
+                <span className="capitalize">{company?.category}</span>
+              </li>
+              <li>
+                <div className="div-row">
+                  <div className="div-row">
+                    <span>Dirección: </span>
+                    <span>{company?.address}</span>
+                  </div>
+                  <div className="div-row">
+                    <span>Telefono: </span>
+                    <span>{practitioner?.phone?.number} </span>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <span>Supervisor Calificador de la Empresa o Institucion:</span>
+                <div className="div-row">
+                  <span>Nombre: </span>
+                  <span className="capitalize">
+                    {fullName(representativeCompany)}
                   </span>
-                  <br />
-                </span>
-              </li>
-              <li>
-                <span>7.</span>
-                <span>
-                  Giro de la Empresa o Institución:&nbsp;
-                  <strong className="capitalize">{company?.category}</strong>
-                </span>
-              </li>
-              <li>
-                <span>8.</span>
-                <span>
-                  Dirección:&nbsp; <strong>{company?.address}</strong>
-                </span>
-              </li>
-              <li>
-                <span></span>
-                <span>
-                  Telefono:&nbsp;{" "}
-                  <strong>{practitioner?.phone?.number} </strong>
-                </span>
-              </li>
-              <li>
-                <span> 9.</span>
-                <span>
-                  Supervisor Calificador de la Empresa o Institucion:&nbsp;
-                </span>
-              </li>
-              <li>
-                <span></span>
-                <span className="capitalize">
-                  Nombre:&nbsp;{" "}
-                  <strong>{fullName(representativeCompany)}</strong>
-                </span>
-              </li>
-              <li>
-                <span></span>
-                <span>
-                  Cargo:&nbsp;{" "}
-                  <strong>
+                </div>
+                <div className="div-row">
+                  <span>Cargo: </span>
+                  <span className="capitalize">
                     {getBusinessPosition(
                       representativeCompany?.companyRepresentativeData
                         ?.businessPosition
                     )?.label || ""}
-                  </strong>
-                </span>
-              </li>
-              <li>
-                <span> 10.</span>
-                <span>Lugares de Práctica:&nbsp;</span>
-              </li>
-              <li className="table_center_work">
-                <div className="center_work">
-                  <table>
-                    <tbody>
-                      {Object.entries(PracticeArea).map(([key, object]) => (
-                        <tr key={key}>
-                          <td>{object.name}</td>
-                          <td>{key === practice?.practiceArea ? "x" : ""}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                  </span>
                 </div>
               </li>
               <li>
-                <span> 11.</span>
+                <span>Lugares de Práctica:</span>
+                <div className="table_center_work">
+                  {Object.entries(PracticeArea).map(([key, object]) => (
+                    <div key={key}>
+                      {object.name}:{" "}
+                      {key === practice?.practiceArea ? "x" : " - "}
+                    </div>
+                  ))}
+                </div>
+              </li>
+              <li>
+                <span>Horario de prácticas: </span> Lunes a Viernes de{" "}
                 <span>
-                  Horario de prácticas:{" "}
-                  <strong>
-                    {dayjs(practice?.entryTime, "HH:mm:ss").format("HH:mm")} -{" "}
-                    {dayjs(practice?.departureTime, "HH:mm:ss").format("HH:mm")}
-                  </strong>
+                  {dayjs(practice?.entryTime, "HH:mm:ss").format("HH:mm")} -{" "}
+                  {dayjs(practice?.departureTime, "HH:mm:ss").format("HH:mm")}
                 </span>
               </li>
               <li>
-                <span> 12.</span>
-                <span>
-                  Tareas asignadas según el modulo indicado: <br />
-                  <br />
-                  <div className="task-box capitalize">
-                    <strong>{practice?.task}</strong>
-                  </div>
-                </span>
+                <span>Tareas asignadas, según el modulo indicado:</span>
+                <div className="task-box capitalize">
+                  <p>{practice?.task}</p>
+                </div>
               </li>
-            </ul>
+            </ol>
 
             <div className="body__subtitle1">
               <h5>II. INSTRUCCIONES PARA LA EVALUACIÓN CUALITATIVA: </h5>
@@ -205,7 +157,7 @@ export const PracticesSheet5 = ({
 
             <ul>
               <li>
-                <span>1.</span>
+                <span>1. </span>
                 <span className="justify">
                   Examine cuidadosamente cada uno de los criterios (A, B, C, D,
                   E, F), de la Ficha de Evaluación adjunta, que consta de 20
@@ -213,39 +165,50 @@ export const PracticesSheet5 = ({
                   cuenta la siguiente tabla de equivalencia
                 </span>
               </li>
+              <li>
+                <span></span>
+                <div className="table-container">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>ESCALA LITERAL</td>
+                        <td>ESCALA CUALITATIVA</td>
+                        <td>ESCALA CUANTITATIVA</td>
+                      </tr>
+                      <tr>
+                        <td>A</td>
+                        <td>Muy Buena</td>
+                        <td>19 a 20</td>
+                      </tr>
+                      <tr>
+                        <td>B</td>
+                        <td>Buena</td>
+                        <td>15 a 18</td>
+                      </tr>
+                      <tr>
+                        <td>C</td>
+                        <td>Aceptable</td>
+                        <td>13 a 14</td>
+                      </tr>
+                      <tr>
+                        <td>D</td>
+                        <td>Deficiente</td>
+                        <td>12 a menos</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </li>
+              <li>
+                <span>2. </span>
+                <span>
+                  Cada indicador se calificará de 0 a 1, se sumarán las veinte
+                  (calificaciones) para obtener el puntaje total, luego se
+                  establecerá las equivalencias con la tabla anterior para
+                  expresar la evaluación final en forma cualitativa y literal.
+                </span>
+              </li>
             </ul>
-
-            <div className="table-container">
-              <table>
-                <tbody>
-                  <tr>
-                    <td>ESCALA LITERAL</td>
-                    <td>ESCALA CUALITATIVA</td>
-                    <td>ESCALA CUANTITATIVA</td>
-                  </tr>
-                  <tr>
-                    <td>A</td>
-                    <td>Muy Buena</td>
-                    <td>19 a 20</td>
-                  </tr>
-                  <tr>
-                    <td>B</td>
-                    <td>Buena</td>
-                    <td>15 a 18</td>
-                  </tr>
-                  <tr>
-                    <td>C</td>
-                    <td>Aceptable</td>
-                    <td>13 a 14</td>
-                  </tr>
-                  <tr>
-                    <td>D</td>
-                    <td>Deficiente</td>
-                    <td>12 a menos</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
           </div>
         </div>
       </Container>
@@ -257,11 +220,15 @@ const Container = styled.div`
   font-size: 14px;
 
   .task-box {
-    width: fit-content;
-    margin: auto;
-    padding: 10px;
+    width: 100%;
+    padding: 1rem;
     border: 1px solid black;
     text-align: center;
+
+    p {
+      margin: 0;
+      font-weight: 700;
+    }
   }
 
   .header {
@@ -306,14 +273,39 @@ const Container = styled.div`
       width: 570px;
       margin: auto;
 
+      .table_center_work {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+      }
+
+      ol {
+        line-height: 1.5;
+
+        li {
+          > span:first-child {
+            font-weight: 700;
+          }
+
+          div {
+            display: flex;
+            flex-direction: column;
+
+            > span:first-child {
+              font-weight: 700;
+            }
+          }
+
+          .div-row {
+            display: flex;
+            flex-direction: row;
+            gap: 1rem;
+          }
+        }
+      }
+
       ul {
         list-style: none;
         line-height: 1.5em;
-
-        .table_center_work {
-          display: flex;
-          justify-content: center;
-        }
 
         li {
           display: grid;
@@ -326,23 +318,6 @@ const Container = styled.div`
 
           .justify {
             text-align: justify;
-          }
-
-          .center_work {
-            table {
-              font-size: 13px;
-              margin: 1em;
-              width: 400px;
-              border-collapse: collapse;
-              text-align: center;
-
-              td {
-                font-weight: bold;
-                width: 50px;
-                border: 1px solid black;
-                padding: 1px;
-              }
-            }
           }
         }
       }
